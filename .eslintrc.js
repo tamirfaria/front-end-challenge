2741b84b27a19583f3734a987cc0078ac95f2865
Lucas Tamir Faria <lucastamir@gmail.com>
2023-08-28 15:56:40 -0300

    :heavy_plus_sign: Adding and setting eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['html'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+};
